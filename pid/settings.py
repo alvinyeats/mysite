08e203b63f0ebeb1435abe39268a6c9c48647ebe
alvin <alvinyeats@gmail.com>
2016-12-09 14:33:57 +0800

    use web proxy by gunicorn

--- pid/settings.py
+++ pid/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ike3)vvfm-99zfil0q*(-od08^9yzkpi###x_)v+47sbsn3e_$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG=DEBUG
 
 ALLOWED_HOSTS = ['wyiren.com', '139.224.197.46', 'localhost', '127.0.0.1']
 
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+
