89734be67bb0b3f939280295707e1bfdd085c14d
alvin <alvinyeats@gmail.com>
2016-12-19 19:23:33 +0800

    fix url

--- article/urls.py
+++ article/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<my_args>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^test/$', views.test, name='test'),
 ]
