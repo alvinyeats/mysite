d46bd952cba4288b659d4b0f924cbb001bbb0722
alvin yeats <alvinyeats@gmail.com>
2016-12-19 18:55:09 +0800

    test arg

--- article/views.py
+++ article/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the article index.")
+
+
+def detail(request, my_args):
+    return HttpResponse("You're looking at my_args %s." % my_args)
