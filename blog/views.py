b879e6b14afe63bc31982eea1763096270748e44
alvin yeats <alvinyeats@gmail.com>
2016-12-19 19:18:20 +0800

    add test

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render,render_to_response
+from datetime import datetime
+
  
 def index(request):
     return render_to_response('index.html')
 
+
+def test(request) :
+    return render(request, 'test.html', {'current_time': datetime.now()})
