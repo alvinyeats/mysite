9b788ace1bb51bfa7cf0d98eae1ef03d3c36f0b9
alvin yeats <alvinyeats@gmail.com>
2016-12-19 19:35:06 +0800

    fix dir

--- article/views.py
+++ article/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from datetime import datetime
+
 from article.models import Article
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the article index.")
@@ -7,6 +12,10 @@ def index(request):
 
 def detail(request, my_args):
     post = Article.objects.all()[int(my_args)]
-    res_str = ("title = %s, category = %s, date_time = %s, content = %s" 
-	% (post.title, post.category, post.date_time, post.content))
+    res_str = ("title = %s, category = %s, date_time = %s, content = %s"
+               % (post.title, post.category, post.date_time, post.content))
     return HttpResponse(res_str)
+
+
+def test(request):
+    return render(request, 'test.html', {'current_time': datetime.now()})
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render,render_to_response
-from datetime import datetime
+from django.shortcuts import render_to_response
 
  
 def index(request):
     return render_to_response('index.html')
 
 
-def test(request) :
-    return render(request, 'test.html', {'current_time': datetime.now()})
