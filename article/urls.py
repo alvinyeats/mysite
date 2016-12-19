5d5fa1d98d319784e4522eb0fa6c6ca240967c8a
alvin yeats <alvinyeats@gmail.com>
2016-12-19 19:47:45 +0800

    add base and hone page

--- article/urls.py
+++ article/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
     url(r'^(?P<my_args>[0-9]+)/$', views.detail, name='detail'),
     url(r'^test/$', views.test, name='test'),
 ]
--- article/views.py
+++ article/views.py
@@ -19,3 +19,8 @@ def detail(request, my_args):
 
 def test(request):
     return render(request, 'test.html', {'current_time': datetime.now()})
+
+
+def home(request):
+    post_list = Article.objects.all()  #获取全部的Article对象
+    return render(request, 'home.html', {'post_list' : post_list})
