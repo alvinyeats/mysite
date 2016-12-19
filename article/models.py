ebe1882bb23424c0c78c1f31cc86a48d06e0317d
alvin yeats <alvinyeats@gmail.com>
2016-12-19 15:25:37 +0800

    fix module funciton

--- article/models.py
+++ article/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)  # 博客题目
     category = models.CharField(max_length=50, blank=True)  # 博客标签
-    post_time = models.DateTimeField(auto_now_add=True)  # 博客日期
+    date_time = models.DateTimeField(auto_now_add=True)  # 博客日期
     content = models.TextField(blank=True, null=True)  # 博客文章正文
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
     class Meta:  # 按时间下降排序
